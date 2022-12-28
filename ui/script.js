01dd09d4f0014552809c4046b7353417483ce2d5
Davide Minatel <davide.minatel@gmail.com>
2022-12-28 02:12:37 +0100

    add dialog shirt

--- ui/script.js
+++ ui/script.js
@@ -7,10 +7,24 @@ function getShirtOfTheWeek() {
     fetch('/getWeeklyShirt').then((resp) => resp.json())
         .then(function (data) {
             document.getElementById("weeklyShirt").src = data.image;
+            document.getElementById("label_weeklyShirt").textContent = data.name;
         });
 
 }
 
+function openFormWeeklyShirt() {
+    const modal = document.querySelector('#modal');
+    fetch('/getWeeklyShirt').then((resp) => resp.json())
+        .then(function (data) {
+            document.getElementById("shirt").src = data.image;
+        });
+    modal.showModal();
+}
+
+function closeFormWeeklyShirt() {
+    const modal = document.querySelector('#modal');
+    modal.close();
+}
 
 function getCompanyImage() {
 
